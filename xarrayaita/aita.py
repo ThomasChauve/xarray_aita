684f1c4a6aff332734c8422a785b8bb3670aa8c3
Thomas Chauve <thomaschauve@disroot.org>
2021-06-03 15:53:46 +0200

    add craft function

--- xarrayaita/aita.py
+++ xarrayaita/aita.py
@@ -5,6 +5,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 from skimage import morphology
 import ipywidgets as widgets
+import scipy
+import datetime
 
 from IPython import get_ipython
 if get_ipython().__class__.__name__=='ZMQInteractiveShell':
@@ -67,7 +69,7 @@ class aita(object):
         new[idx,idy,:]=np.nan
         self._obj.orientation[:,:,:]=new
         
-        
+#--------------------------------------------------------------------------------------------            
     def crop(self,lim,rebuild_gId=True):
         '''
         :param rebuild_gId: recompute the grainID
@@ -81,6 +83,73 @@ class aita(object):
             ds.grainId.data=morphology.label(ds.micro, connectivity=1, background=1)
         
         return ds
+#--------------------------------------------------------------------------------------------    
+    def craft(self,nameId,res=0):
+        '''
+        Export 'vtk' file and the phase file. 
+        :param res: resolution for the vtk export
+        :type res: float
+        '''
+        zoom=self._obj.step_size/res
+        
+        # Copy and compute mean grain
+        ds = self._obj.copy(deep=False)
+        ds['orientation_mg']=ds.aita.mean_grain()
+        # Remove hole in grainId
+        new_g = scipy.ndimage.maximum_filter(np.array(ds.grainId), 3)
+        # Resize grainId for wrting vtk
+        new_g_rs=scipy.ndimage.interpolation.zoom(new_g,zoom,order=0,mode='nearest')
+        # extract euler angle for phase file
+        euler_ori=ds.orientation_mg.uvecs.bunge_euler()
+        
+        ################################   
+        # Write the microstructure input
+        ################################
+        # size of the map
+        ss=np.shape(new_g_rs)
+        # open micro.vtk file
+        micro_out=open(nameId+'_micro.vtk','w')
+        # write the header of the file
+        micro_out.write('# vtk DataFile Version 3.0 ' + str(datetime.date.today()) + '\n')
+        micro_out.write('craft output \n')
+        micro_out.write('ASCII \n')
+        micro_out.write('DATASET STRUCTURED_POINTS \n')
+        micro_out.write('DIMENSIONS ' + str(ss[1]) + ' ' + str(ss[0]) +  ' 1\n')
+        micro_out.write('ORIGIN 0.000000 0.000000 0.000000 \n')
+        micro_out.write('SPACING ' + str(res) + ' ' + str(res) + ' 1.000000 \n')
+        micro_out.write('POINT_DATA ' + str(ss[0]*ss[1]) + '\n')
+        micro_out.write('SCALARS scalars float \n')
+        micro_out.write('LOOKUP_TABLE default \n')
+        for i in list(range(ss[0]))[::-1]:
+            for j in list(range(ss[1])):
+                micro_out.write(str(int(new_g_rs[i,j]))+' ')
+            micro_out.write('\n')        
+        micro_out.close()
+        
+        phase_out=open(nameId+'.phase','w')
+        phase_out.write('#------------------------------------------------------------\n')
+        phase_out.write('# Date ' + str(datetime.date.today()) + '      Manip: ' + nameId + '\n')
+        phase_out.write('#------------------------------------------------------------\n')
+        phase_out.write('# This file give for each phase \n# *the matetial \n# *its orientation (3 euler angles)\n')
+        phase_out.write('#\n#------------------------------------------------------------\n')
+        phase_out.write('# phase    material       phi1    Phi   phi2\n')
+        phase_out.write('#------------------------------------------------------------\n')
+        for i in tqdm(np.unique(new_g_rs)):
+            sub_ds=ds.where(ds.grainId==i)
+            if int(sub_ds.grainId.sum())!=0:
+                id1,id2=np.where(np.array(ds.grainId)==i)
+                phi1=float(euler_ori[id1[0],id2[0],0])
+                phi=float(euler_ori[id1[0],id2[0],1])
+                phi2=float(np.random.rand(1)*2*np.pi)
+                if np.isnan(phi1):
+                    phi1=float(np.random.rand(1)*2*np.pi)
+                    phi=float(np.random.rand(1)*2*np.pi)
+                phase_out.write(str(i) + '          0              ' + str(phi1) + ' ' + str(phi) + ' ' + str(phi2) + '\n');  
+        phase_out.close()
+        
+        
+        
+        
     
 #--------------------------------------------------------------------------------------------    
     def mean_grain(self,dilate=True):
