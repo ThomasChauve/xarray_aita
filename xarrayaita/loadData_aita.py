9d193637f0f1d2107a92285176651d07a541c418
Thomas Chauve <thomaschauve@disroot.org>
2021-06-18 09:43:46 +0200

    add loading from craft

--- xarrayaita/loadData_aita.py
+++ xarrayaita/loadData_aita.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 from skimage import io
 from skimage import morphology
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+import scipy
 
 def aita5col(adr_data,micro_adress=0):
     '''
@@ -130,4 +133,59 @@ def aita3col(adr_data,image,micro_adress=0):
     ds.attrs["unit"]='millimeters'
     ds.attrs["step_size"]=resolution
     ds.attrs["path_dat"]=adr_data
+    return ds
+
+def craft_input(adr_vtk,adr_phase):
+    '''
+    Function to load the data from craft simulation
+    
+    :param adr_vtk: path to the vtk
+    :type adr_data: str
+    :param phase: path to phase
+    :type phase: str
+    '''
+    reader = vtk.vtkDataSetReader() 
+    reader.SetFileName(adr_vtk)
+    reader.Update()
+    ug  = reader.GetOutput()
+    res=ug.GetSpacing()[0]
+    ugdim=ug.GetDimensions()
+    map=vtk_to_numpy(ug.GetPointData().GetScalars()).reshape((ug.GetDimensions()[0:2][::-1]))
+    
+    orientation=np.transpose(np.loadtxt(adr_phase,unpack=True, skiprows=9, usecols=(0,2,3,4),dtype='f,f,f,f'))
+    
+    phi1=np.zeros(map.shape)
+    phi=np.zeros(map.shape)
+    
+    for i in np.unique(map):
+        phi1[map==i]=orientation[orientation[:,0]==i,1][0]
+        phi[map==i]=orientation[orientation[:,0]==i,2][0]
+
+    azi=np.mod(phi1-np.pi/2,2*np.pi)
+    
+    micro=(scipy.signal.convolve2d(map,np.array([[0,-1,0],[-1,4,-1],[0,-1,0]]))>0)[1:-1,1:-1]
+    
+    ss=map.shape
+    
+    dy=np.linspace(0,ss[0]-1,ss[0])*res
+    dx=np.linspace(0,ss[1]-1,ss[1])*res
+    
+    ds = xr.Dataset(
+    {   
+        "orientation": (["y", "x","uvecs"],np.dstack((azi,phi))),
+        "quality": (["y", "x"],np.ones(azi.shape)*100),
+        "micro": (["y", "x"],micro),
+        "grainId": (["y", "x"], map),
+        
+    },
+    coords={
+        "x": dx,
+        "y": dy,
+    },
+    )
+    
+    ds.attrs["unit"]='millimeters'
+    ds.attrs["step_size"]=res
+    ds.attrs["path_dat"]=adr_vtk
+                    
     return ds
